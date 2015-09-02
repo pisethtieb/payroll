7b89541246ce76a50a6e9875e3fc290b6b68d57c
Yuom Theara <yuom.theara@gmail.com>
2015-09-02 10:46:31 +0700

    fix and update

--- packages/autoprint/package.js
+++ packages/autoprint/package.js
@@ -1,6 +1,6 @@
 Package.describe({
     name: 'theara:autoprint',
-    version: '0.0.5',
+    version: '0.0.6',
     // Brief, one-line summary of the package.
     summary: '',
     // URL to the Git repository containing the source code for this package.

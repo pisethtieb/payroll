704154fd36b95686c720e02646e7545a24f57fb4
Yuom Theara <yuom.theara@gmail.com>
2015-10-03 09:48:00 +0700

    update sample fixtures

--- sample/server/fixtures/_createFactory.js
+++ sample/server/fixtures/_createFactory.js
@@ -1,6 +1,6 @@
 Meteor.startup(function () {
     if (Sample.Collection.Customer.find({}).count() == 0) {
-        _.times(5, function (n) {
+        _.times(1000, function (n) {
             Factory.create('customer');
         });
     }
